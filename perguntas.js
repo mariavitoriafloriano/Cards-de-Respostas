06013433aae8cbbd6da83969e6e99e9cfff44449
mariavitoriafloriano <00001105395029sp@al.educacao.sp.gov.br>
2024-10-27 14:08:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Serie',
+    'Qual minha serie favorita?',
+    'Supernatural'
+)
+
+criaCartao(
+    'Jogo Favorito',
+    'Qual meu jogo favorito?',
+    'The Last of Us'
+)
+
+criaCartao(
+    'Cantora',
+    'Qual é minha cantora favorita?',
+    'Lana Del Rey'
+)
+
+criaCartao(
+    'Animal',
+    'Meu animal favorito?',
+    'Raposa'
+)
